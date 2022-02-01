6952663888f0dc4fca983632f5c41d3838a4653e
maysara <maysarax@users.noreply.github.com>
2022-02-01 11:21:33 +0200

    update.1.2.2022

--- createpdf.php
+++ createpdf.php
@@ -1,33 +1,33 @@
 <?php
     
-require('fpdf.php');
+    require('fpdf.php');
 
-function createPDF($content)
-{
-    if (!isset($GLOBALS["pdf_font"])) 
-	{
-        $GLOBALS["pdf_font"] = 'Arial';
-        $GLOBALS["pdf_fontsize"] = 12;
-		$GLOBALS["pdf_fontstyle"]='normal';
-    }
-	echo '$content';
-	//require('htmloutput.php');
-
-	$pdf=new PDF_HTML();
-
-    $pdf->AliasNbPages();
-    $pdf->AddPage();
-
-    //$pdf->SetFont($GLOBALS["pdf_font"], $GLOBALS["pdf_fontstyle"], $GLOBALS["pdf_fontsize"]);
-
-	//$pdf->Write((int) $GLOBALS["pdf_fontsize"] / 2, $content);
-	//$htmloutput='';
-	
-	//$pdf-> WriteHTML  ("<br><br>$htmloutput");
-    $fname = tempnam($GLOBALS["tmpdir"], "pdf");
-    $pdf->Output($fname, false);
- 
-    return $fname;
+    function createPDF($content)
+    {
+        if (!isset($GLOBALS["pdf_font"])) 
+        {
+            $GLOBALS["pdf_font"] = 'Arial';
+            $GLOBALS["pdf_fontsize"] = 12;
+            $GLOBALS["pdf_fontstyle"]='normal';
+        }
+        echo '$content';
+        //require('htmloutput.php');
+    
+        $pdf=new PDF_HTML();
+    
+        $pdf->AliasNbPages();
+        $pdf->AddPage();
+    
+        //$pdf->SetFont($GLOBALS["pdf_font"], $GLOBALS["pdf_fontstyle"], $GLOBALS["pdf_fontsize"]);
+    
+        //$pdf->Write((int) $GLOBALS["pdf_fontsize"] / 2, $content);
+        //$htmloutput='';
+        
+        //$pdf-> WriteHTML  ("<br><br>$htmloutput");
+        $fname = tempnam($GLOBALS["tmpdir"], "pdf");
+        $pdf->Output($fname, false);
+     
+        return $fname;
 
 
 	
